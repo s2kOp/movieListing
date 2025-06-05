cc9dc6e6498e0811a334c2b17fac728a44968ff8
rohitrn_ <rohitrajeev05@gmail.com>
2025-06-05 13:07:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+import {useState,useEffect} from "react"
+import  "./App.css"
+
+function App(){
+  const [name,setName] = useState("");
+  const [moviesList,setMoviesList] = useState([]);
+  const handleSearch = async () => {
+    if(name==""){
+      console.error("No name provided...");
+    }
+    else{
+      try{
+        const response = await fetch(`http://www.omdbapi.com/?s=${name}&apikey=117aea9a`);
+        if(response.ok){
+          const result = await response.json();
+          setMoviesList(result.Search);
+        }
+      }catch(err){
+        console.error(err);
+      }finally{
+        setName("");
+      }
+    }
+  }
+
+  
+
+  return(
+    <div>
+      <div className="headPart">
+          <h1>MovieSearch</h1>
+          <p>Discover, rate, and explore the world of cinema – all in one place.</p>
+      </div>        
+      <div className="inputData">
+          <input type = "text" placeholder="Enter movie name..." value = {name} onChange={(e)=>setName(e.target.value)}></input>
+          <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="movieList">
+        {moviesList.map((item)=>(
+            <div className="movieCards" key={item.imdbID}>
+              <img src = {item.Poster} alt = {"unable to load"}></img>
+              <h4>{item.Title}</h4>
+              <p>{item.Year}</p>
+            </div>
+        ))}
+      </div>
+    </div>
+        )
+}
+export default App;
